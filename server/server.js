e18504a58881db595aabeee3f12bdcada0983918
KaranNegi0102 <karannegi0102@gmail.com>
2025-02-28 19:27:02 +0530

    pushed backend

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || '*',
     methods: ['GET', 'POST'],
   },
 });
@@ -15,9 +15,7 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Video chat server running');
-});
+app.get('/', (req, res) => res.send('Video chat server running'));
 
 let connectedUsers = [];
 
@@ -25,30 +23,22 @@ io.on('connection', (socket) => {
   const user = { id: socket.id };
   connectedUsers.push(user);
   console.log('User connected:', socket.id);
-  console.log('Current users:', connectedUsers.map((u) => u.id)); // Debug log
+  console.log('Current users:', connectedUsers.map((u) => u.id));
   io.emit('user-list', connectedUsers.map((u) => u.id));
 
-  socket.on('offer', (data) => {
-    socket.broadcast.emit('offer', data);
-  });
-
-  socket.on('answer', (data) => {
-    socket.broadcast.emit('answer', data);
-  });
-
-  socket.on('ice-candidate', (data) => {
-    socket.broadcast.emit('ice-candidate', data);
-  });
+  socket.on('offer', (data) => socket.broadcast.emit('offer', data));
+  socket.on('answer', (data) => socket.broadcast.emit('answer', data));
+  socket.on('ice-candidate', (data) => socket.broadcast.emit('ice-candidate', data));
 
   socket.on('disconnect', () => {
     connectedUsers = connectedUsers.filter((u) => u.id !== socket.id);
     console.log('User disconnected:', socket.id);
-    console.log('Current users:', connectedUsers.map((u) => u.id)); // Debug log
+    console.log('Current users:', connectedUsers.map((u) => u.id));
     io.emit('user-list', connectedUsers.map((u) => u.id));
   });
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app; // Export for Vercel
