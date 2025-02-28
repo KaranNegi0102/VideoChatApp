106b39ccdfdf75008f8e17ac9c9c589b22ba3f68
KaranNegi0102 <karannegi0102@gmail.com>
2025-02-28 23:02:32 +0530

    changes done

--- client/src/components/VideoChat.js
+++ client/src/components/VideoChat.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io(process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000',{reconnection : true});
 
 const VideoChat = () => {
   const localVideoRef = useRef(null);
